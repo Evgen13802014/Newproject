4df6981d42b0c7d7e46a577be5122e5e845ce5aa
Evgen13802014 <64891358+evgen13802014@users.noreply.github.com>
2020-06-04 11:08:37 +0300

    Delete тренировка.py

--- тренировка.py
+++
@@ -1,5 +0,0 @@
-Age =int (input (" Введие ваш возраст :"))
-first_name = input (" Введите ваше имя: ")
-Second_name = input("Введите вашу фамилию : ")
-
-print (f'Добро пожаловать ,{first_name} {Second_name} , ваш возраст {Age} года')
